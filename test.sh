60e89dd8c210d74d9a437e4ae2f8f6bc08f2574b
burniechen <>
2022-08-17 13:02:46 +0800

    [FILE] add ppt

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@ make clean
 make
 # ASAN_OPTIONS=detect_leaks=1 ./bench
 
- ASAN_OPTIONS=detect_leaks=1 ./each --dir="~/dlrm-file/dlrm/rmc2/" \
+# ASAN_OPTIONS=detect_leaks=1 ./each --dir="~/dlrm-file/dlrm/rmc2/" \
 	--embedding-size="500000" \
 	--feature-size="64" \
 	--num-indices-per-lookup="120" \
 	--type="io_buf"
 
-# ASAN_OPTIONS=detect_leaks=1 ./ratio
+ASAN_OPTIONS=detect_leaks=1 ./ratio
